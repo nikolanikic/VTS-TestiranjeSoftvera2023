74ab75ed7256fe7f3954f67d6eec2e206e84c94f
nikolanikic <nikolanikic11@gmail.com>
2023-04-04 10:03:12 +0200

    Updates

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    chromeWebSecurity: false,
+    watchForFileChanges: false,
   },
 });
