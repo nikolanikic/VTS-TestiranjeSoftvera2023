d436a7b8412eda4b4a01fc00a12ea81a73f610c4
adelmurselji <103389379+adelmurselji@users.noreply.github.com>
2023-05-15 16:24:12 +0200

    Cucumber implementing(Google.feature,google.js)

---
+++ cypress/e2e/am_136/Google/google.js
@@ -0,0 +1,22 @@
+import { Given, Then } from "cypress-cucumber-preprocessor/steps";
+
+const url = 'https://google.com'
+
+Given('I open Google page', () => {
+  cy.visit(url)
+})
+
+Then('I can see Gmail link',()=>{
+    cy.get(':nth-child(1) > .gb_t').should('have.text','Gmail');
+})
+
+Given('I search rsd to eur', () => {
+    cy.get('#APjFqb').clear().type('rsd to eur{enter}');
+  })
+
+Given('I enter {int} for original currency', (original) => {
+    cy.get("[type='number']").eq(0).clear().type(original);
+  })
+Given('I should get 1.71 in new currency', () => {
+    cy.get("[type='number']").eq(1).clear().should('be.visible');
+  })
