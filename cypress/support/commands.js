792ad8f3479e9463abc6ff7e9c4f12748615bc6e
aleksavlatkovic <aleksa.vlatkovic@gmail.com>
2023-06-05 15:44:08 +0200

    Petsorteapi

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,78 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('addPet',(petId,petName,photoUrl) =>{
+    cy.request('POST', 'https://petstore.swagger.io/v2/pet', {
+        "id": petId,
+        "category": {
+            "id": 0,
+            "name": "string",
+        },
+        "name": petName,
+        "photoUrls": [
+            photoUrl
+        ],
+        "tags": [
+            {
+                "id": 0,
+                "name": "string"
+            }
+        ],
+        "status": "available"
+    }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.property('id', petId);
+        expect(response.body).to.have.property('name', petName);
+    }
+    );
+
+});
+
+Cypress.Commands.add('findPet',(petId)=>{
+    cy.request('GET','https://petstore.swagger.io/v2/pet/'+petId).then((response) =>{
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.property('id', petId);
+    })
+});
+
+Cypress.Commands.add('updatePet',(petId,petNewName,photoUrl) =>{
+    cy.request('POST', 'https://petstore.swagger.io/v2/pet', {
+        "id": petId,
+        "category": {
+            "id": 0,
+            "name": "string",
+        },
+        "name": petNewName,
+        "photoUrls": [
+            photoUrl
+        ],
+        "tags": [
+            {
+                "id": 0,
+                "name": "string"
+            }
+        ],
+        "status": "available"
+    }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.property('id', petId);
+        expect(response.body).to.have.property('name', petNewName);
+    }
+    );
+
+});
+
+Cypress.Commands.add('deletePet',(petId)=>{
+    cy.request('DELETE','https://petstore.swagger.io/v2/pet/'+petId).then((response) =>{
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.property('message','111');
+    })
+});
+
+Cypress.Commands.add('deleteFakePet',(petId)=>{
+    cy.request('DELETE','https://petstore.swagger.io/v2/pet/'+petId).then((response) =>{
+        expect(response.status).to.eq(404);
+    })
+});
