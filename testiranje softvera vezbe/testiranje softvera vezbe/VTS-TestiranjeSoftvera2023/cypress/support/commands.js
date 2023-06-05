b325211bee0438ec44b74b208dd41e1c93851449
Magdalena Jaksic <64920092+Magdalena3333@users.noreply.github.com>
2023-06-05 14:13:30 +0200

    Add files via upload

--- testiranje softvera vezbe/testiranje softvera vezbe/VTS-TestiranjeSoftvera2023/cypress/support/commands.js
+++ testiranje softvera vezbe/testiranje softvera vezbe/VTS-TestiranjeSoftvera2023/cypress/support/commands.js
@@ -1,28 +1,5 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import { expect } from "chai";
+
 Cypress.Commands.add('addPet', (petId, petName, photoURL) => {
     cy.request('POST', 'https://petstore.swagger.io/v2/pet', {
         "id": petId,
@@ -100,3 +77,15 @@ Cypress.Commands.add('findPetAndSaveToFile', (petId) => {
         cy.writeFile('cypress/fixtures/pet.json', response.body)
     })
 });
+
+Cypress.Commands.add('deleteProjectByCode', (projectCode, token) => {
+    cy.request({
+        method: "DELETE",
+        url: "https://api.qase.io/v1/project/TN" + projectCode,
+        headers: {
+            Token: token
+        }
+    }).then((response) => {
+        expect(response.status).to.eq(200);
+     })
+})
