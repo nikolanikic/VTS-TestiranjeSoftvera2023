23ee71c29292e7dae50c8ccb4b2648e9ee8d2d1d
Veljko <mr.veljkovic11@gmail.com>
2023-06-10 18:55:08 +0200

    Promene

---
+++ cypress/e2e/vv_076/demoQA.cy.js
@@ -0,0 +1,40 @@
+// <reference types="cypress" />
+
+describe("Testing DemoQA", () => {
+
+    it.skip("Elements - testing text box", () => {
+        cy.visit("https://demoqa.com/");
+
+        cy.get(':nth-child(1) > :nth-child(1) > .card-up').click();
+        cy.get(':nth-child(1) > .element-list > .menu-list > #item-0').click();
+
+        cy.get('.main-header').should('have.text', 'Text Box');
+
+        cy.wait(2000);
+        cy.get('#userName-label').should('have.text', 'Full Name');
+        cy.get('#userEmail-label').should('have.text', 'Email');
+
+        cy.get('#userName').type('Veljko Veljkovic');
+        cy.get('#userEmail').type('mr.veljkovic11@gmail.com');
+        cy.get('#currentAddress').type('Slavka Zlatanovica bb');
+        cy.get('#permanentAffress').type('Slavka Zlatanovica 88');
+    });
+
+    it("Elements - testing radio button", () => {
+        cy.visit("https://demoqa.com/");
+
+        cy.get(':nth-child(1) > :nth-child(1) > .card-up').click();
+        cy.contains('Radio Button').click();
+
+        cy.get('.main-header').should('have.text', 'Radio Button');
+
+        cy.get('.col-md-6 > :nth-child(2) > :nth-child(2)').click();
+        cy.get('.text-success').should('have.text', 'Yes');
+
+        cy.get('.col-md-6 > :nth-child(2) > :nth-child(3)').click();
+        cy.get('.text-success').should('have.text', 'Impressive');
+
+        cy.get('.custom-control.disabled').should('be.disabled');
+    });
+
+});
---
+++ cypress/e2e/vv_076/demoblaze.cy.js
@@ -0,0 +1,83 @@
+// <reference types="cypress" />
+
+import SlideShow from "../../support/pageObject/demoblaze_ms_127/SlideShow";
+import HomePage from "../../support/pageObject/demoblaze_ms_127/homePage";
+import LogInPage from "../../support/pageObject/demoblaze_ms_127/logInPage";
+import SignUpPage from "../../support/pageObject/demoblaze_ms_127/signUpPage";
+
+
+
+describe("Demoblaze web shop", () => {
+
+    const homePage = new HomePage();
+    const signUpPage = new SignUpPage();
+    const logInPage = new LogInPage();
+    const slideShow = new SlideShow();
+
+    it("User is able to load Demoblaze web shop", () => {
+        cy.visit('https://www.demoblaze.com/');
+
+        homePage.checkLogoText();
+        homePage.checkHomeButtonIsDisplayed();
+        homePage.checkCategoriesMenuIsDisplayed();
+    });
+
+    it("Alert is thrown when User already exists", () => {
+        cy.visit('https://www.demoblaze.com/');
+        homePage.clickSignUpButton();
+
+        signUpPage.checkSignUpLabel();
+        signUpPage.populateAndSubmitSignUpForm();
+
+        cy.on('window:alert', (t) => {
+            expect(t).to.contains('This user already exist.');
+        });
+    });
+
+    it('User is able to login to Demoblaze web shop', () => {
+        cy.visit('https://www.demoblaze.com/');
+        homePage.clickLogInButton();
+        logInPage.fillAndSubmitLogInForm();
+
+    });
+
+
+    it.skip("Adding product to cart", () => {
+        cy.visit("https://www.demoblaze.com/");
+
+        cy.get(':nth-child(3) > .card > .card-block .card-title > .hrefch').click();
+        cy.get('.name').contains('Nexus 6');
+        cy.get('.col-sm-12 > .btn').click();
+
+        cy.get('#cartur').click();
+        cy.get('#totalp').should('have.text', '650');
+        cy.get('.col-lg-1 > .btn').click();
+
+        cy.get('#totalm').contains('650');
+        cy.get('#name').clear().type('Veljko Veljkovic');
+        cy.get('#country').clear().type('Serbia');
+        cy.get('#city').clear().type('Leskovac');
+        cy.get('#card').clear().type('384664620487');
+        cy.get('#month').clear().type('05');
+        cy.get('#year').clear().type('24');
+
+        cy.get('#orderModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click();
+        cy.get('.confirm').click();
+        cy.get('#orderModal > .modal-dialog > .modal-content > .modal-footer > .btn-secondary').click();
+        cy.on('window:alert', (t) => {
+            expect(t).to.contains('Product added');
+        });
+
+    });
+    it("Check if slide is changed after click", () => {
+
+        cy.visit("https://www.demoblaze.com/");
+        slideShow.checkFirstSlide();
+        cy.wait(4000);
+        slideShow.checkSecondSlide();
+
+
+    });
+
+
+});
